58b99b1ad1950f5917b03ea17b488dfc5f625d89
jotaefecueme <50833571+jotaefecueme@users.noreply.github.com>
2025-06-05 02:53:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -117,14 +117,14 @@ async def classify(req: ClassificationRequest, request: Request):
     result, infer_time = await classify_input(req.user_input, req.intents, req.entities)
 
     now = datetime.now()
-    date_str = now.strftime("%Y-%m-%d") 
+    date_obj = now.date() 
     time_str = now.strftime("%H:%M:%S")
 
     ip = request.client.host
 
     asyncio.create_task(insert_log(
         ip=ip,
-        date=date_str,
+        date=date_obj,
         log_time=time_str,
         input_text=req.user_input,
         input_intents=req.intents,
